7796b95e0d9c9932c1293dae61b034a82bde07ac
mits <dmitrybaycharov@gmail.com>
2021-04-05 16:30:21 -0500

    part 8: adding session finished

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash
+from flask import Flask, render_template, request, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
@@ -12,17 +12,19 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
 
-responses = []  #empty list initiated for user to fill
-
 @app.route('/')
 def home():
-    responses = [] #reset responses    
     return render_template('home.html', survey=survey)
+
+@app.route('/init', methods=['POST'])
+def start_survey():
+    session['responses'] = []
+    return redirect('/question/0')
 
 @app.route('/question/<int:qidx>')
 def question(qidx):
     #protect from user tinkering with URL
-    correct_idx = len(responses)
+    correct_idx = len(session['responses'])
     if qidx != correct_idx:
         flash(f'Sir/madam, you are trying to access an invalid question and have been redirected to question {correct_idx + 1}.')
         return redirect(f'/question/{correct_idx}')
@@ -33,10 +35,10 @@ def question(qidx):
 @app.route('/answer', methods=['POST'])
 def answer():
     answ = request.form['choice']
-    if len(responses) < len(survey.questions): #only append if responses not yet full
-        responses.append(answ)
+    if len(session['responses']) < len(survey.questions): #only append if responses not yet full
+        session['responses'] = [*session['responses'], answ] #append response
 
-    qidx = len(responses)
+    qidx = len(session['responses'])
     if qidx > len(survey.questions) - 1:
         return redirect('/thankyou') 
     else:
@@ -44,8 +46,11 @@ def answer():
 
 @app.route('/thankyou')
 def thankyou():
-    print(responses) 
+    print(session['responses']) 
     return render_template('thankyou.html')
 
 
     
+@app.before_request
+def test():
+    print(session.get('responses', None))
